6cad8ccdaaab64b3ab8f6b4b8a760d75ebe7ad4f
7DREZZ <99985897+7DREZZ@users.noreply.github.com>
2022-04-14 20:39:20 +0300

    second commit

--- sql3.py
+++ sql3.py
@@ -14,7 +14,6 @@ query2 = "CREATE TABLE IF NOT EXISTS students (id INT NOT NULL PRIMARY KEY, fami
 query3 = "CREATE TABLE IF NOT EXISTS subjects (id INT NOT NULL PRIMARY KEY, subject TEXT)" 
 query4 = "CREATE TABLE IF NOT EXISTS marks (id INT NOT NULL PRIMARY KEY, mark INT, id_students INT, id_subjects INT, FOREIGN KEY (id_subjects) REFERENCES subjects (id) ON UPDATE CASCADE, FOREIGN KEY (id_students) REFERENCES student (id) ON UPDATE CASCADE)"
 
-#query3 = "CREATE TABLE IF NOT EXISTS  (Id INT, class INT, num_of_stud INT, teacher INT,)"
 cur.execute(query1)
 cur.execute("INSERT INTO classes VALUES(1, '5A', 21, 'Pakhomov Dmitry Davidovich')")
 cur.execute("INSERT INTO classes VALUES(2, '9B', 22, 'Pavlov Roman Makarovich')")
@@ -48,26 +47,26 @@ cur.execute("INSERT INTO students VALUES(922, 'Maxwell', 'Lisa', 'Alekseevna', 1
 cur.execute("INSERT INTO students VALUES(923, 'Hart', 'Mary', 'Vladimirovna', 13, 'female', 15)")
 cur.execute("INSERT INTO students VALUES(924, 'McCormick', 'Pamela', 'Olegovna', 14, 'female', 15)")
 cur.execute("INSERT INTO students VALUES(925, 'Williams', 'Christine', 'Romanovna', 15, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(926, 'Gilbert', 'Erika', 'Timofeevna', 16, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(927, 'Lee', 'Phillip', 'Ilyich', 17, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(928, 'Miller', 'Bernard', 'Yegorovich', 18, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(929, 'Moore', 'Henry', 'Vladislavovich', 19, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(930, 'Turner', 'Tracy', 'Ivanovna', 20, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(931, 'Gutierrez', 'Cathy', 'Alexandrovna', 21, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(932, 'Williams', 'Deborah', 'Ivanovich', 22, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(933, 'Hardy', 'Aaron', 'Zakharovich', 23, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(934, 'Adams', 'Paul', 'Romanovich', 24, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(935, 'Waters', 'Matthew', 'Ruslanovich', 25, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(936, 'Miller', 'Maria', 'Romanovna', 26, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(937, 'Brown', 'Judy', 'Nikolaevna', 27, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(938, 'Fowler', 'Darryl', 'Alexandrovich', 28, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(939, 'West', 'Andrew', 'Maksimovich', 29, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(940, 'Butler', 'Michael', 'Mikhailovich', 30, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(941, 'Jenkins', 'Joseph', 'Yegorovich', 31, 'male', 15)")
-cur.execute("INSERT INTO students VALUES(942, 'Elliott', 'Crystal', 'Alexeyevna', 32, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(943, 'Malone', 'Mary', 'Vsevolodovna', 33, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(944, 'Fields', 'Peggy', 'Mikhailovna', 34, 'female', 15)")
-cur.execute("INSERT INTO students VALUES(945, 'Perkins', 'Justin', 'Alexandrovich', 35, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(926, 'Gilbert', 'Erika', 'Timofeevna', 1, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(927, 'Lee', 'Phillip', 'Ilyich', 2, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(928, 'Miller', 'Bernard', 'Yegorovich', 3, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(929, 'Moore', 'Henry', 'Vladislavovich', 4, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(930, 'Turner', 'Tracy', 'Ivanovna', 5, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(931, 'Gutierrez', 'Cathy', 'Alexandrovna', 6, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(932, 'Williams', 'Deborah', 'Ivanovich', 7, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(933, 'Hardy', 'Aaron', 'Zakharovich', 8, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(934, 'Adams', 'Paul', 'Romanovich', 9, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(935, 'Waters', 'Matthew', 'Ruslanovich', 10, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(936, 'Miller', 'Maria', 'Romanovna', 11, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(937, 'Brown', 'Judy', 'Nikolaevna', 12, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(938, 'Fowler', 'Darryl', 'Alexandrovich', 13, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(939, 'West', 'Andrew', 'Maksimovich', 14, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(940, 'Butler', 'Michael', 'Mikhailovich', 15, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(941, 'Jenkins', 'Joseph', 'Yegorovich', 1, 'male', 15)")
+cur.execute("INSERT INTO students VALUES(942, 'Elliott', 'Crystal', 'Alexeyevna', 2, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(943, 'Malone', 'Mary', 'Vsevolodovna', 3, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(944, 'Fields', 'Peggy', 'Mikhailovna', 4, 'female', 15)")
+cur.execute("INSERT INTO students VALUES(945, 'Perkins', 'Justin', 'Alexandrovich', 5, 'male', 15)")
 
 cur.execute(query3)
 cur.execute("INSERT INTO subjects VALUES(601, 'Russian language')")
@@ -83,70 +82,69 @@ cur.execute("INSERT INTO marks VALUES(114, 4, 13, 603)") #
 cur.execute("INSERT INTO marks VALUES(115, 4, 14, 604)") #
 cur.execute("INSERT INTO marks VALUES(116, 5, 15, 605)") #
 
-#cur.execute("SELECT * FROM classes")
-#rows = cur.fetchall() 
-#for row in rows:
-#    print(row)
-#print(" ")
-
-cur.execute("SELECT * FROM students")
+print("Все поля из таблицы Студенты:")
+cur.execute("SELECT * FROM students") # Вывод всех полей из таблицы Студенты
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-cur.execute("SELECT imya, familia, otchestvo FROM students")
+print("Поля Имя, Фамилия, Отчество из таблицы Студенты:")
+cur.execute("SELECT imya, familia, otchestvo FROM students") # Вывод полей Имя, Фамилия, Отчество из таблицы Студенты
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-cur.execute("SELECT * FROM students WHERE age=15 LIMIT 30")
+print("Первые 30 полей из таблицы Студенты, возраст которых составляет 15 лет:")
+cur.execute("SELECT * FROM students WHERE age=15 LIMIT 30") # Вывод Первых 30 полей из таблицы Студенты, возраст которых составляет 15 лет
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-cur.execute("SELECT * FROM classes WHERE class<>'5A' AND num_of_stud>25")
+print("Все поля из таблицы Классы, кроме 5A и которые имеют больше 25 студентов в группе:")
+cur.execute("SELECT * FROM classes WHERE class<>'5A' AND num_of_stud>25") # Вывод всех полей из таблицы Классы, кроме 5A, имеющие больше 25 студентов в группе
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-cur.execute("SELECT * FROM classes WHERE num_of_stud BETWEEN 20 AND 27")
+print("Все поля из таблицы Классы, в которых количество студентов от 20 до 27:")
+cur.execute("SELECT * FROM classes WHERE num_of_stud BETWEEN 20 AND 27") # Вывод всех полей из таблицы Классы, в которых количество студентов от 20 до 27
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-
-
-cur.execute("SELECT * FROM classes WHERE teacher<>'Shatilova Tatiana Nikolaevna' AND id>=7 LIMIT 5")
+print("Первые 5 полей, начиная с 7, из таблицы Классы, кроме тех, где учитель Шатилова Татьяна Николаева:")
+cur.execute("SELECT * FROM classes WHERE teacher<>'Shatilova Tatiana Nikolaevna' AND id>=7 LIMIT 5") # Вывод первых 5 полей, начиная с 7, из таблицы Классы, кроме тех, где учитель Шатилова Татьяна Николаева
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
 print(" ")
+print(" ")
 
-cur.execute("SELECT imya, familia, otchestvo FROM students WHERE age<18")
+print("Фаимилия, имя и отчество из таблицы Студенты тех учеников, кому еще не исполнилось 18 лет:")
+cur.execute("SELECT imya, familia, otchestvo FROM students WHERE age<18") # Вывод фаимилии, имени и отчества из таблицы Студенты тех учеников, кому еще не исполнилось 18 лет
 rows = cur.fetchall() 
 for row in rows:
+    print(" ")
     print(row)
+print(" ")
 print(" ")
 
 
-
-
-#cur.execute("SELECT * FROM subjects")
-#ows = cur.fetchall() 
-#for row in rows:
-#    print(row)
-#print(" ")
-
-#cur.execute("SELECT * FROM marks")
-#rows = cur.fetchall() 
-#for row in rows:
-#    print(row)
-#print(" ")
-
-#connect.commit()
-#connect.close()
+connect.commit()
+connect.close()
